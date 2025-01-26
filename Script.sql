787671ab4213f57efe4f756db2ec5710cd5b34c1
Abdallah Mostafa <abdallah.mostafa.draz@gmail.com>
2025-01-26 15:54:06 +0200

    Initial Commit

---
+++ Script.sql
@@ -0,0 +1,43 @@
+-- Omnia 
+--------------------
+-- 1. Department 
+
+-- 2. Branches	
+	
+-- 3. Tracks
+	
+-- 4. Instructor
+
+-----------------------------------
+
+-- Reyad
+---------------------
+-- 5. Student 
+
+-- 6. Courses
+
+-- 7. CourseTracks
+
+-- 8. CousreInstructors
+
+----------------------------------------
+
+-- Hakim
+------------------------
+-- 9. CourseStudents
+
+-- 10. Questions
+
+-- 11. Choices
+
+------------------------------------------
+
+-- Abdallah 
+------------------------
+-- 12. Exams
+
+-- 13. ExamQuestions
+
+-- 14. StudentAnswers
+
+-- 15. StudentExmas
