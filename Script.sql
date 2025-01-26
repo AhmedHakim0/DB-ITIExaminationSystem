4a6bc61e239098c54f1fe5039516ab961a2c33e4
Abdallah Mostafa <abdallah.mostafa.draz@gmail.com>
2025-01-26 17:10:00 +0200

    Create Department, Branch, BranchDepartments, and Track Tables

--- Script.sql
+++ Script.sql
@@ -1,13 +1,31 @@
 -- Omnia 
 --------------------
 -- 1. Department 
+Create Table Department
+(dept_Id int primary key identity,
+dept_Name varchar(25) unique,
+mngr_Id int )
 
 -- 2. Branches	
-
--- 3. BranchesDepratment
-
+Create Table Branch
+(br_Id int primary key identity,
+br_Name varchar(25) unique, 
+br_Location varchar(100),
+br_phone varchar(25),
+mngr_Id int
+)
+-- 3. BrancheDepratments
+create table BrancheDepartments
+(br_Id int foreign key references Branch(br_Id),
+dept_Id int foreign key references Department(dept_Id),
+constraint Branch_Dept_PK primary key (br_Id, dept_Id) 
+)
 -- 4. Tracks
-	
+create Table Track
+(trk_Id int primary key identity, 
+trk_Name varchar(25),
+Dept_Id int foreign key references Department(dept_Id)
+)
 
 -----------------------------------
 
